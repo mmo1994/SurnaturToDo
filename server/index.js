d1f6a4ac75900a8641c3406c46a57795fec4eec5
Sargis Manasyan <manasyansak286@gmail.com>
2025-04-15 00:55:22 +0400

    WITHOUT CRED

--- server/index.js
+++ server/index.js
@@ -13,8 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 const corsOptions = {
-	origin: '*', // Allow all origins
-	credentials: true,
+	origin: '*',
 	optionsSuccessStatus: 200
 };
 
