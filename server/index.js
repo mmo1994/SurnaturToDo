f6bd003c05654cdd4b6614e2b328a80e815822cf
Sargis Manasyan <manasyansak286@gmail.com>
2025-04-14 23:52:11 +0400

    Fix

--- server/index.js
+++ server/index.js
@@ -12,8 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 // Custom CORS middleware
 app.use((req, res, next) => {
+	//process.env.FRONTEND_URL
 	// Allow specific origins or use * for development
-	const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
+	const allowedOrigins = ['https://surnatur-to-do.vercel.app', 'http://localhost:3000'];
 	const origin = req.headers.origin;
 
 	if (allowedOrigins.includes(origin)) {
